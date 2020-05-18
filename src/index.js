4b1cbcf55fa5cba4d14169f9b1d951dbb6e214fb
Shivam Srivastava <48158198+shivam-ss@users.noreply.github.com>
2020-05-18 20:00:56 +0530

    committing variables via react

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const name = "Sakshi ";
+
 ReactDOM.render(
   <div>
-    <h1> hello world </h1>
+    <h1>
+      {" "}
+      Hello {name}, your favorite number is {Math.ceil(2 + 5)}{" "}
+    </h1>
     <ul>
       <li> TEA </li>
       <li> coffeee </li>
