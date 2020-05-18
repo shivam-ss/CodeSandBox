ab75167aea01572b4f00b5df2f75545cdc72862e
Shivam Srivastava <48158198+shivam-ss@users.noreply.github.com>
2020-05-18 19:48:46 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1> hello world </h1>
+    <ul>
+      <li> TEA </li>
+      <li> coffeee </li>
+      <li> Water </li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
